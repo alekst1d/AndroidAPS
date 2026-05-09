da352788e5234221e4a44adda307796164beed61
Aleksandar <sasa_obu@gmx.de>
2026-05-09 14:18:55 +0200

    for Omnipod Eros don't disable the Loop for 3h after DST changes (untested, but see comment)

--- pump/omnipod/eros/src/main/java/app/aaps/pump/omnipod/eros/OmnipodErosPumpPlugin.kt
+++ pump/omnipod/eros/src/main/java/app/aaps/pump/omnipod/eros/OmnipodErosPumpPlugin.kt
@@ -766,7 +766,14 @@ class OmnipodErosPumpPlugin @Inject constructor(
     }
 
     override val isFakingTempsByExtendedBoluses: Boolean = false
-    override fun canHandleDST(): Boolean = false
+
+    /*
+    If (potentially) larger time zone changes while traveling are no problem, I don't see why a DST change should cause the Loop
+    to be disabled for 3h?!
+    Also I never had any problems with DST changes while Looping wih Omnipod on iOS and the Loop isn't automatically disabled
+    for DST changes in iOS Loop either
+    */
+    override fun canHandleDST(): Boolean = true
     override fun finishHandshaking() {
         if (displayConnectionMessages) aapsLogger.debug(LTag.PUMP, "finishHandshaking [OmnipodPumpPlugin] - default (empty) implementation.")
     }
